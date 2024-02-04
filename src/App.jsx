09b51dbfb9ff400f4f7661c37d4287d14634777a
Lucas Alecsander <lucasalecsander10@hotmail.com>
2024-02-04 02:04:00 -0300

    Rendering Content Conditionally

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,21 @@ function App() {
     console.log()
   }, [selectedTopic])
 
-  console.log(selectedTopic);
+  console.log('APP COMPONENT EXECUTING');
+
+  let tabContent = <p>Please select a topic.</p>;
+
+  if (selectedTopic) {
+    tabContent = (
+      <div id="tab-content">
+        <h3>{EXAMPLES[selectedTopic].title}</h3>
+        <p>{EXAMPLES[selectedTopic].description}</p>
+        <pre>
+          <code>{EXAMPLES[selectedTopic].code}</code>
+        </pre>
+      </div>
+    );
+  }
 
   return (
     <div>
@@ -58,14 +72,7 @@ function App() {
             <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
             <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
           </menu>
-          <div id='tab-content'>
-            <h3>{EXAMPLES[selectedTopic].title}</h3>
-            <p>{EXAMPLES[selectedTopic].description}</p>
-            <pre>
-              <code>{EXAMPLES[selectedTopic].code}</code>
-            </pre>
-
-          </div>
+          {tabContent}
         </section>
       </main>
     </div>
